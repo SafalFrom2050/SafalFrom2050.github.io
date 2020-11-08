4c2fe8a5b3bfb66ffd01044e8bbc26a0a1c1add8
Safal Sharma <safal.sharma.ss@gmail.com>
2020-11-08 16:26:46 +0545

    Fixed Bugs

--- cookiePopUp.js
+++ cookiePopUp.js
@@ -1,11 +1,3 @@
-  var config = {
-    apiKey: "AIzaSyCa21G1mEhrJKmoPLRZ8hbJikyI4lGdY5Y",
-    authDomain: "dif-instantgames.firebaseapp.com",
-    databaseURL: "https://dif-instantgames.firebaseio.com/",
-	storageBucket: "dif-instantgames.appspot.com"
-  };
-  firebase.initializeApp(config);
-
 $(document).ready(function(){   
     setTimeout(function () {
         $("#cookieConsent").fadeIn(200);
@@ -14,24 +6,3 @@ $(document).ready(function(){
         $("#cookieConsent").fadeOut(200);
     }); 
 });
-
-  function signIn(){
-	var provider = new firebase.auth.GoogleAuthProvider();
-	firebase.auth().signInWithPopup(provider).then(function(result) {
-		// This gives you a Google Access Token. You can use it to access the Google API.
-		var token = result.credential.accessToken;
-		// The signed-in user info.
-		var user = result.user;
-		// ...
-	  }).catch(function(error) {
-		// Handle Errors here.
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		// The email of the user's account used.
-		var email = error.email;
-		// The firebase.auth.AuthCredential type that was used.
-		var credential = error.credential;
-		// ...
-	  });
-	
-  }
